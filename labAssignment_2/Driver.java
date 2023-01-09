8fa3759e032ee49c4ab3ae761bb5d3ee76217d04
Ashu-sk <74344531+Ashu-sk@users.noreply.github.com>
2023-01-09 20:42:39 +0530

    Add files via upload

---
+++ labAssignment_2/Driver.java
@@ -0,0 +1,13 @@
+package com.labAssignment_2;
+
+public class Driver {
+ public static void main(String argc[]){
+	        CurrencyDenominations currencyDenominations = new CurrencyDenominations();
+
+	        currencyDenominations.countCurrency();
+	    }
+
+	}
+
+
+
